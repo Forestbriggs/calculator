cfdd5b349737af0893ab448e912bb6fc7f08314a
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-03-31 21:25:55 -0700

    most functionality added, still need some bug fixes and decimal implementation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,44 +1,97 @@
 const display = document.querySelector('#display');
 
+let chained = '';
 let firstNum = '';
 let secNum = '';
 let op = '';
+let lastSecNum = '';
+let lastOp = '';
+
+//TODO fix issue where after calculate typing adds numbers to the end of firstNum
+//TODO   instead of secNum
+
+//TODO fix issue where calculations after error messages return NaN
+
+//TODO implement decimal points
+
+
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
         if (e.target.classList.contains('num')) {
+            if (op && secNum === '') {
+                display.textContent = e.target.id;
+            } else {
+                display.textContent += e.target.id;
+            }
+
             if (op) {
                 secNum += e.target.id;
             } else {
                 firstNum += e.target.id;
             };
-
-            if (display.textContent.includes('+') ||
-                display.textContent.includes('-') ||
-                display.textContent.includes('÷') ||
-                display.textContent.includes('x')) {
-                display.textContent = e.target.id;
-            } else {
-                display.textContent += e.target.id
-            }
-            console.log('firstNum', firstNum)
-            console.log('sec', secNum)
         } else if (e.target.classList.contains('operator')) {
+
+            if (op) {
+                chained = operate(op, parseFloat(firstNum), parseFloat(secNum));
+                display.textContent = chained;
+                firstNum = `${chained}`;
+                secNum = '';
+            }
             op = e.target.id;
-            switch (op) {
-                case 'add':
-                    display.textContent = '+';
-                    return;
-                case 'subtract':
-                    display.textContent = '-';
-                    return;
-                case 'multiply':
-                    display.textContent = 'x';
-                    return;
-                case 'divide':
-                    display.textContent = '÷';
-                    return;
+
+        } else if (e.target.id === 'calculate') {
+
+            if (op === 'divide' && (secNum === '0' || secNum === '-0')) {
+                clear();
+                display.textContent = 'nuh uh';
+                return;
+            }
+
+            if (secNum) {
+                res = operate(op, parseFloat(firstNum), parseFloat(secNum));
+                lastSecNum = `${secNum}`;
+                secNum = '';
+                lastOp = `${op}`;
+                op = '';
+            } else {
+                res = operate(lastOp, parseFloat(firstNum), parseFloat(lastSecNum));
+            }
+            firstNum = `${res}`;
+            display.textContent = res;
+
+        } else if (e.target.id === 'clear') {
+            clear();
+        } else if (e.target.id === 'switcher') {
+            if (!secNum) {
+                if (firstNum.startsWith('-')) {
+                    firstNum = firstNum.substring(1);
+                    display.textContent = firstNum;
+                } else {
+                    firstNum = '-' + firstNum;
+                    display.textContent = firstNum;
+                }
+            } else {
+                if (secNum.startsWith('-')) {
+                    secNum = firstNum.substring(1);
+                    display.textContent = secNum;
+                } else {
+                    secNum = '-' + secNum;
+                    display.textContent = secNum;
+                }
+            }
+        } else if (e.target.id === 'percent') {
+            if (firstNum) {
+                const percentage = parseFloat(display.textContent) / 100;
+
+                if (!secNum) {
+                    firstNum = `${percentage}`;
+                    display.textContent = firstNum;
+                } else {
+                    secNum = `${percentage}`;
+                    display.textContent = secNum;
+                }
             }
         }
     })
@@ -73,8 +126,18 @@ const operate = (operator, num1, num2) => {
     }
 
     if (!Number.isNaN(ans) && ans % 1 !== 0) {
-        ans = ans.toFixed(6)
+        ans = parseFloat(ans.toFixed(6));
     }
 
-    display.textContent = ans;
+    return ans;
 };
+
+function clear() {
+    firstNum = '';
+    secNum = '';
+    op = '';
+    chained = '';
+    lastSecNum = '';
+    lastOp = '';
+    display.textContent = '';
+}
